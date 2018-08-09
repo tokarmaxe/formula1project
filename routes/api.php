e051ec52841cde7cccc11212fd9318d9d9ab280b
qwer <as>
2018-08-10 01:20:18 +0300

    remove google tag from route

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,4 @@ use Illuminate\Http\Request;
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
 //    return $request->user();
 //});
-Route::post('loginbygoogle', 'UserController@singUpByGoogle')->name('user.login');
+Route::post('logining', 'UserController@Logging')->name('user.login');
