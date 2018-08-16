6005509c86c948d98e63a4f69adbd0a876ed475b
Andrey Sklyanichenko <sklyack@gmail.com>
2018-08-16 23:32:13 +0300

    fix some bugs

--- app/Components/User/Services/UserService.php
+++ app/Components/User/Services/UserService.php
@@ -66,18 +66,17 @@ class UserService implements UserServiceContract
      */
     private function checkPayloadEmail($payload)
     {
-        if (empty($payload['email'])) {
+        if ( ! $payload['email']) {
             throw new AuthenticationException('E-mail is not available');
         }
         //TODO array with allowed email domains '@provectus.com' + all emails from Baraholka doc
         $row = explode('@', $payload['email']);
-        if (!(in_array($row[1],
-            Config::get('services.allowed_email_domains'))==true)
+        if ( ! (in_array($row[1],
+                Config::get('services.allowed_email_domains')) == true)
         ) {
             throw new AuthenticationException('E-mail domain is not allowed');
         }
-
-        return true;
+        
     }
 
 
