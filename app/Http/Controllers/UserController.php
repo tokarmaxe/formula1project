5efe716d706cc8eac0b856679b6943fb8d5015f9
Andrey Sklyanichenko <sklyack@gmail.com>
2018-08-22 23:54:15 +0300

    return user info from controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,26 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Components\User\Services\UserServiceContract;
+use App\Components\User\Models\UserContract;
 USE App;
 
 
 class UserController extends Controller
 {
     /**
-     * @param Request $request
+     * @param Request             $request
      * @param UserServiceContract $userService
+     *
      * @return \Illuminate\Http\JsonResponse
      */
     public function login(Request $request, UserServiceContract $userService)
     {
         $result = $userService->login($request);
-
         return $this->sendResponse($result);
     }
 
-    public function user()
+    public function user(Request $request, UserContract $user)
     {
-        dd('hello');
-        return $this->sendResponse(['some data']);
+        $apiToken = $request->header('authorization');
+        $apiToken = str_replace('Bearer ', '', $apiToken);
+        return
+            $this->sendResponse($user->where('api_token', $apiToken)->first()
+                ->toArray());
     }
 }
