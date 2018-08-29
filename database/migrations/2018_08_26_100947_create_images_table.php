83bf7306372226aeccb74daa4541ae1cf7b4f850
Sklyack <sklyack@gmail.com>
2018-08-29 22:34:29 +0300

    migrations. All it works

--- database/migrations/2018_08_26_100947_create_images_table.php
+++ database/migrations/2018_08_26_100947_create_images_table.php
@@ -14,7 +14,7 @@ class CreateImagesTable extends Migration
     public function up()
     {
         Schema::create('images', function (Blueprint $table) {
-            $table->increments('id')-unique();
+            $table->increments('id')->unique();
             $table->string('name')->unique();
             $table->string('path');
             $table->unsignedInteger('post_id');
