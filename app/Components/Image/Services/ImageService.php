4ce70156069fb69b98b2b7b49dcd29e09bbcaf49
Maxim Tokar <tokarmaxe@gmail.com>
2018-09-30 01:05:12 +0300

    resolved all discussions

--- app/Components/Image/Services/ImageService.php
+++ app/Components/Image/Services/ImageService.php
@@ -2,42 +2,46 @@
 
 namespace App\Components\Image\Services;
 
-use App\Components\File\Services\FileService;
 use App\Components\File\Services\FileServiceContract;
 use App\Components\Image\Models\Image;
+use App\Components\Post\Models\Post;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 
 class ImageService implements ImageServiceContract
 {
     private $image;
     private $fileService;
+    private $postService;
 
-    public function __construct(Image $image, FileServiceContract $file)
+    public function __construct(Image $image, FileServiceContract $file, Post $post)
     {
         $this->image = $image;
         $this->fileService = $file;
+        $this->postService = $post;
     }
 
     public function create($files, $postId)
     {
-        $result = null;
+        if ($this->postService->where('id', $postId)->exists()) {
+            $result = null;
 
-        foreach ($files['images'] as $file)
-        {
-            $data['name']=$file->getClientOriginalName();
-            $data['post_id']=$postId;
-            $data['path']=$this->fileService->put($file, $data['name']);
-            $result[] = $this->image->create($data);
+            foreach ($files['images'] as $file) {
+                $data['name'] = $file->getClientOriginalName();
+                $data['post_id'] = $postId;
+                $data['path'] = $this->fileService->put($file, $data['name']);
+                $result[] = $this->image->create($data);
+            }
+            return $result;
         }
-
-        return $result;
+        throw new ModelNotFoundException('That post does not exist');
     }
 
     public function destroy($imageId)
     {
-            $this->image = $this->image->find($imageId)->firstOrFail();
-            $this->fileService->remove($this->image['path']);
-            return ['success' => $this->image->delete()];
+        $this->image = $this->image->find($imageId)->firstOrFail();
+        $this->fileService->remove($this->image['path']);
+        return ['success' => $this->image->delete()];
 
     }
 }
