ba674d2dc74aaef539c6bb82f6253e6e7ff51bd8
Maxim Tokar <tokarmaxe@gmail.com>
2018-10-02 20:58:30 +0300

    resolved

--- app/Components/Image/Services/ImageService.php
+++ app/Components/Image/Services/ImageService.php
@@ -5,7 +5,6 @@ namespace App\Components\Image\Services;
 use App\Components\File\Services\FileServiceContract;
 use App\Components\Image\Models\Image;
 use App\Components\Post\Models\Post;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Intervention\Image\ImageManagerStatic as InterventionImage;
 use Illuminate\Support\Facades\Config;
 
@@ -24,26 +23,23 @@ class ImageService implements ImageServiceContract
 
     public function create($files, $postId)
     {
-        if ($this->postService->where('id', $postId)->exists()) {
-            $result = null;
-            $height = null;
-            $types = Config::get('services.types');
-            foreach ($files['images'] as $file) {
-                $count = 0;
-                foreach ($types as $type => $typeParams) {
-                    $data['type'] = array_keys($types)[$count++];
-                    $data['name'] = $data['type'] . '-' . $file->getClientOriginalName();
-                    $data['post_id'] = $postId;
-                    $data['path'] = $this->fileService->put($file, $data['name']);
-                    $result[] = $this->image->create($data);
-                    $height = (array_get($typeParams, 'height') == 0) ? getimagesize($file)[1] : array_get($typeParams, 'height');
-                    $image = InterventionImage::make(storage_path($data['path']))->heighten($height);
-                    $image->save(storage_path($data['path']));
-                }
+        $result = null;
+        $height = null;
+        $types = Config::get('services.types');
+        foreach ($files['images'] as $file) {
+            $count = 0;
+            foreach ($types as $type => $typeParams) {
+                $data['type'] = $type;
+                $data['name'] = $data['type'] . '-' . $file->getClientOriginalName();
+                $data['post_id'] = $postId;
+                $data['path'] = $this->fileService->put($file, $data['name']);
+                $result[] = $this->image->create($data);
+                $height = (array_get($typeParams, 'height') == 0) ? getimagesize($file)[1] : array_get($typeParams, 'height');
+                $image = InterventionImage::make(storage_path($data['path']))->heighten($height);
+                $image->save(storage_path($data['path']));
             }
-            return $result;
         }
-        throw new ModelNotFoundException('That post does not exist');
+        return $result;
     }
 
     public function destroy($imageId)
