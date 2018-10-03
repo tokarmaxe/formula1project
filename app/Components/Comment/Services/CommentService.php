eb3227d06fb67ad127bfdccfff3a9d6a914ed933
Maxim Tokar <tokarmaxe@gmail.com>
2018-10-03 15:54:25 +0300

    prepare comments structures

---
+++ app/Components/Comment/Services/CommentService.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Components\Comment\Services;
+
+
+class CommentService implements CommentServiceContract
+{
+
+}
---
+++ app/Components/Comment/Services/CommentServiceContract.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Components\Comment\Services;
+
+interface CommentServiceContract
+{
+
+}
