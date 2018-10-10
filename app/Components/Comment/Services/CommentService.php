9a0555efa38fb602129f709aae6d86bf1596d87d
Maxim Tokar <tokarmaxe@gmail.com>
2018-10-11 00:11:15 +0300

    add DI and add findOrFail for method list

--- app/Components/Comment/Services/CommentService.php
+++ app/Components/Comment/Services/CommentService.php
@@ -3,18 +3,20 @@
 namespace App\Components\Comment\Services;
 
 use App\Components\Comment\Models\Comment;
+use App\Components\Post\Models\Post;
 use App\Components\User\Models\User;
 use App\Exceptions\PermissionDeniedException;
 use Auth;
 
 class CommentService implements CommentServiceContract
 {
-    private $comment, $user;
+    private $comment, $user, $post;
 
-    public function __construct(Comment $comment, User $user)
+    public function __construct(Comment $comment, User $user, Post $post)
     {
         $this->comment = $comment;
         $this->user = $user;
+        $this->post = $post;
     }
 
     public function store($data)
@@ -48,6 +50,7 @@ class CommentService implements CommentServiceContract
 
     public function list($postId)
     {
+        $this->post->findOrFail($postId);
         return $this->comment->where('post_id', $postId)->get()->toArray();
     }
 }
