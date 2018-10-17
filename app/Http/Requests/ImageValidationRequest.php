62c13fd384b5e284a3030ad2d82dd3cf06903a35
Andrey Sklyanichenko <sklyack@gmail.com>
2018-10-17 22:21:52 +0300

    rollback

--- app/Http/Requests/ImageValidationRequest.php
+++ app/Http/Requests/ImageValidationRequest.php
@@ -6,22 +6,14 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ImageValidationRequest extends BaseValidationRequest
 {
-
-    public function rules()
-    {
-    	if(  !$this->route()->uri('api/image_thumb')) {
-		    return [
-			    'post_id' => 'required|exists:posts,id',
-			    'images' => 'required|array',
-			    'images.*' => 'image|mimes:jpeg,png,jpg',
-		    ];
-	    }
-	    else{
-		    return [
-			    'images' => 'required|array',
-			    'images.*' => 'image|mimes:jpeg,png,jpg',
-		    ];
-    		
-	    }
-    }
+	
+	public function rules()
+	{
+		return [
+			'post_id' => 'required|exists:posts,id',
+			'images' => 'required|array',
+			'images.*' => 'image|mimes:jpeg,png,jpg',
+		];
+	}
 }
+
