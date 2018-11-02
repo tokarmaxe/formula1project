c6f2dc030926babf0b9d9d1ed0e59474b470dc6d
Sklyack <sklyack@gmail.com>
2018-11-02 16:09:11 +0200

    revome auto generated code

--- app/Convention/Model/Contracts/IsoDateContract.php
+++ app/Convention/Model/Contracts/IsoDateContract.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sklyack
- * Date: 31.10.18
- * Time: 15:21
- */
 
 namespace App\Convention\Model\Contracts;
 
@@ -12,6 +6,7 @@ namespace App\Convention\Model\Contracts;
 interface IsoDateContract
 {
     public function getCreatedAtAttribute($date);
+
     public function getUpdatedAtAttribute($date);
 
 }
--- app/Convention/Model/Traits/IsoDateTrait.php
+++ app/Convention/Model/Traits/IsoDateTrait.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: sklyack
- * Date: 31.10.18
- * Time: 15:17
- */
 
 namespace App\Convention\Model\Traits;
 
@@ -12,11 +6,14 @@ use Carbon\Carbon;
 
 trait IsoDateTrait
 {
-    public function getCreatedAtAttribute($date){
+    public function getCreatedAtAttribute($date)
+    {
         $date = new Carbon($date);
         return $date->toIso8601String();
     }
-    public function getUpdatedAtAttribute($date){
+
+    public function getUpdatedAtAttribute($date)
+    {
         $date = new Carbon($date);
         return $date->toIso8601String();
     }
