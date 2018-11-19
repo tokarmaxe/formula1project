a213013b621bd77932e84289d69dd420c0a99f55
Maxim Tokar <tokarmaxe@gmail.com>
2018-11-20 01:52:39 +0200

    remove useless code

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Components\User\Models\User;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
