790b9b6461afe9734df7895d2e8e13b7fb418baa
Sklianichenko Andrii <sklyack@gmail.com>
2019-02-04 20:53:33 +0100

    return one thumbnail are impl

--- app/Convention/Model/Traits/ImageTrait.php
+++ app/Convention/Model/Traits/ImageTrait.php
@@ -7,7 +7,16 @@ use App\Components\Image\Models\Image;
 
 trait ImageTrait
 {
-    public function getDeleteImages($postId, $str)
+    /*
+     * this method can work with files connected with post -
+     *  1) returns files array by post id
+     * -only all thumnails,
+     * - all files without origin,
+     * - only one thumbnail by post
+     * - all files
+     * 2)delete files by post id
+     */
+    public function getDeleteImages($postId, $str, bool $lazy = true)
     {
         $cnt = 0;
         if($str == "getThumb")
@@ -22,7 +31,7 @@ trait ImageTrait
                 return $item['type'];
             },
         ], $preserveKeys = true)
-            ->mapWithKeys(function ($item) use (&$cnt, &$str) {
+            ->mapWithKeys(function ($item) use (&$cnt, &$str, &$lazy) {
                 $i = $item->map(function ($subItems) use(&$str) {
                     switch ($str){
                         case ($str == "get" || $str == "getWithOutOrigin" || $str == "getAll" || $str == "getThumb"):
@@ -34,8 +43,10 @@ trait ImageTrait
                             break;
                     }
                 });
-
                 $subResult = [$cnt => $i];
+                //we returned only one thumbnail for every post if $lasy is true - not ,more need to build list of posts
+                if ($lazy&&$str=='getThumb'&&!is_null($subResult))
+                    return $subResult;
                 $cnt++;
                 return $subResult;
             })->toArray();
